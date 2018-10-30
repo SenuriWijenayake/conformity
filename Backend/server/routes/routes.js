3da531fc9c2b1fd3ec653cf66bde5f46cc4e3017
Senuri Wijenayake <swijenayake@4180L-143992-M.local>
2018-10-31 10:02:58 +1100

    Before mongodb

--- Backend/server/routes/routes.js
+++ Backend/server/routes/routes.js
@@ -1,6 +1,6 @@
 var logic = require('../code');
 
-var appRouter = function (app) {
+var appRouter = function(app) {
   app.get('/chartData/:question/:id/:answer/:confidence', function(req, res) {
 
     var userAnswer = {};
@@ -25,10 +25,10 @@ var appRouter = function (app) {
   //Endpoint to process the big five data
   app.get('/bigFiveData', function(req, res) {
     console.log("Request received at big five");
-    //answerData = JSON.stringify(req.body);
     response = logic.processBigFive();
     res.status(200).send("<h2>Thank You!</h2><p>Responses were submited successfully</p>");
   });
-}
+
+};
 
 module.exports = appRouter;
