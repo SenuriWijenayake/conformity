fcc9e0bc813b55ccee3ed7d2e3718f5d47d0e392
Senuri Wijenayake <swijenayake@4180L-143992-M.local>
2018-11-13 00:44:43 +1100

    Fixing the required fields issue

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -14,6 +14,7 @@ app.controller('BigFiveController', function($scope, $http) {
 
 app.controller('HomeController', function($scope, $http, $window) {
   $scope.submitDetails = function(user) {
+    console.log(user);
     $http({
       method: 'POST',
       url: api + '/user',
@@ -29,10 +30,21 @@ app.controller('HomeController', function($scope, $http, $window) {
 });
 
 app.controller('QuizController', function($scope, $http, $window) {
+
+  $window.onbeforeunload = function(e) {
+      var dialogText = 'You have unsaved changes. Are you sure you want to leave the site?';
+      e.returnValue = dialogText;
+      return dialogText;
+    };
+
   $scope.count = 0;
   $scope.myAnswer = {};
-  $scope.myAnswer.confidence = 1;
+  $scope.myAnswer.confidence = 50;
   $scope.userId = $window.sessionStorage.getItem('userId');
+
+  $scope.clicked = function() {
+    $("#confidence-container").css("display", "block");
+  };
 
   $scope.question = {
     "questionNumber": 0,
@@ -54,9 +66,14 @@ app.controller('QuizController', function($scope, $http, $window) {
   };
 
   $scope.submitAnswer = function() {
-    if ($scope.myAnswer.confidence != 1) {
-      //Disable the button
-      $("#submit-button").attr("disabled", "disabled");
+    if ($scope.myAnswer.confidence != 50) {
+
+      //Remove the button
+      $("#submit-button").css("display", "none");
+      //Disbling the input
+      $("input[type=radio]").attr('disabled', true);
+      $("input[type=range]").attr('disabled', true);
+      //Loader activated
       $("#loader").css("display", "block");
       $("#loader-text").css("display", "block");
 
@@ -115,8 +132,53 @@ app.controller('QuizController', function($scope, $http, $window) {
       // Set chart options
       var options = {
         'width': 640,
-        'height': 500
+        'height': 500,
+        'vAxis': {
+          'ticks': [{
+              v: 0,
+              f: '0%'
+            }, {
+              v: 10,
+              f: '10%'
+            }, {
+              v: 20,
+              f: '20%'
+            }, {
+              v: 30,
+              f: '30%'
+            },
+            {
+              v: 40,
+              f: '40%'
+            },
+            {
+              v: 50,
+              f: '50%'
+            },
+            {
+              v: 60,
+              f: '60%'
+            },
+            {
+              v: 70,
+              f: '70%'
+            },
+            {
+              v: 80,
+              f: '80%'
+            },
+            {
+              v: 90,
+              f: '90%'
+            },
+            {
+              v: 100,
+              f: '100%'
+            }
+          ]
+        }
       };
+
       // Instantiate and draw our chart, passing in some options.
       var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
       chart.draw(data, options);
@@ -125,13 +187,19 @@ app.controller('QuizController', function($scope, $http, $window) {
 
   $scope.yes = function() {
     $scope.count = 1;
-    $("#submit-button").prop("disabled", false);
+    //Make the submit button visible and the input enabled
+    $("input[type=radio]").attr('disabled', false);
+    $("input[type=range]").attr('disabled', false);
+    $("#submit-button").css("display", "block");
+
+    //Remove change section buttons
     $("#change-section").css("display", "none");
   };
 
   $scope.update = function() {
     //Disable the button
     $("#submit-button").attr("disabled", "disabled");
+    $("#confidence-container").css("display", "none");
 
     $scope.myAnswer.answerId = parseInt($scope.myAnswer.answerId);
     $scope.myAnswer.questionId = $scope.question.questionNumber;
@@ -149,10 +217,18 @@ app.controller('QuizController', function($scope, $http, $window) {
     });
   };
 
-  $scope.next = function (){
+  $scope.next = function() {
     $scope.count = 0;
+    //Make the submit button visible and the input enabled
+    $("input[type=radio]").attr('disabled', false);
+    $("input[type=range]").attr('disabled', false);
+    $("#submit-button").css("display", "block");
+    $("#confidence-container").css("display", "none");
+
     $scope.userId = $window.sessionStorage.getItem('userId');
-    var data = {id : parseInt($scope.myAnswer.questionId) + 1};
+    var data = {
+      id: parseInt($scope.myAnswer.questionId) + 1
+    };
 
     $http({
       method: 'POST',
@@ -161,15 +237,15 @@ app.controller('QuizController', function($scope, $http, $window) {
       type: JSON,
     }).then(function(response) {
 
-        $scope.myAnswer = {};
-        $scope.myAnswer.confidence = 1;
-        $scope.question = response.data;
+      $scope.myAnswer = {};
+      $scope.myAnswer.confidence = 50;
+      $scope.question = response.data;
 
-        $("#loader").css("display", "none");
-        $("#loader-text").css("display", "none");
-        $("#chart_div").css("display", "none");
-        $("#change-section").css("display", "none");
-        $("#submit-button").prop("disabled", false);
+      $("#loader").css("display", "none");
+      $("#loader-text").css("display", "none");
+      $("#chart_div").css("display", "none");
+      $("#change-section").css("display", "none");
+      $("#submit-button").prop("disabled", false);
 
     }, function(error) {
       console.log("Error occured when loading the question");
